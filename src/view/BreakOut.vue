408a47fd1785b72a5adf1e6fb2366cbaeb4fd017
daikidaiki <daiki.1101.aji@icloud.com>
2025-10-07 17:48:04 +0900

    変更

--- src/view/BreakOut.vue
+++ src/view/BreakOut.vue
@@ -1,7 +1,11 @@
-<script></script>
+<script src="https://cjrtnc.leaningtech.com/2.3/loader.js"></script>
+<script>
+  cheerpjInit();
+  cheerpjRunJar("game.jar");
+</script>
 
 <template>
-  <h1>避けゲー画面(消してOK)</h1>
+  <h1>ブロック崩しゲー</h1>
   <router-link to="/">
     <button>Homeへ遷移</button>
   </router-link>
